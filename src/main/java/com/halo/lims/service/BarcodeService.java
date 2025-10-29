18cb2b3e312c12d1e17e6b78e82500a989aacd10
Srinivas <srinivaschakravarthy1996@gmail.com>
2025-10-29 21:14:15 +0530

    barcode shortened

--- src/main/java/com/halo/lims/service/BarcodeService.java
+++ src/main/java/com/halo/lims/service/BarcodeService.java
@@ -4,7 +4,7 @@ import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
-import com.google.zxing.oned.Code128Writer;
+import com.google.zxing.oned.Code39Writer; // Changed from Code128Writer
 import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayOutputStream;
@@ -15,8 +15,8 @@ import java.util.Base64;
 public class BarcodeService {
 
     public String generateBarcodeImageBase64(String barcodeText) throws WriterException, IOException {
-        Code128Writer barcodeWriter = new Code128Writer();
-        BitMatrix bitMatrix = barcodeWriter.encode(barcodeText, BarcodeFormat.CODE_128, 300, 150);
+        Code39Writer barcodeWriter = new Code39Writer(); // Changed from Code128Writer
+        BitMatrix bitMatrix = barcodeWriter.encode(barcodeText, BarcodeFormat.CODE_39, 300, 150); // Changed from BarcodeFormat.CODE_128
 
         ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
         MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream);
