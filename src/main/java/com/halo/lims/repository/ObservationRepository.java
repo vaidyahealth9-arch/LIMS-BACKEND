2fea705aa8e989f1d94b012018e19d23b3cfb940
Srinivas <srinivaschakravarthy1996@gmail.com>
2025-11-02 19:41:39 +0530

    observation view edit

--- src/main/java/com/halo/lims/repository/ObservationRepository.java
+++ src/main/java/com/halo/lims/repository/ObservationRepository.java
@@ -4,11 +4,12 @@ import com.halo.lims.model.Observation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ObservationRepository extends JpaRepository<Observation, Integer> {
-    Optional<Observation> findByServiceRequestId(Integer serviceRequestId);
+    List<Observation> findByServiceRequestId(Integer serviceRequestId);
 
     Optional<Observation> findTopByServiceRequestIdOrderByEffectiveDateTimeDesc(Integer serviceRequestId);
 }
