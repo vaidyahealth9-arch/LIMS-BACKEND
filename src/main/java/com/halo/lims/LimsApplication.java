0f53b0c0dcf31e1b2c38fe2520af5218e9d76494
vaidyahealth9-arch <vaidyahealth9@gmail.com>
2026-04-25 20:03:11 +0530

    fix: force server address and add startup confirmation log

--- src/main/java/com/halo/lims/LimsApplication.java
+++ src/main/java/com/halo/lims/LimsApplication.java
@@ -3,11 +3,24 @@ package com.halo.lims;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+
 @SpringBootApplication
 public class LimsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LimsApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			System.out.println("=======================================");
+			System.out.println("  LIMS APPLICATION FULLY STARTED!      ");
+			System.out.println("  Listening on Port: " + System.getenv("PORT"));
+			System.out.println("=======================================");
+		};
 	}
 
 }
